b30a42aa6c964ad4fda227edb978379b1e630870
lingaraj2020 <lingarajhonasani2020@gmail.com>
2024-06-07 19:54:55 +0530

    completed 2nd tutorial

--- App.js
+++ App.js
@@ -13,10 +13,12 @@
  *
  * ReactElement(Object) => HTML(Browser Understands)
  */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h1", {}, "this is namaste react!"),
       React.createElement("h2", {}, "I'm an h2 tag"),
     ]),
     React.createElement("div", { id: "child2" }, [
