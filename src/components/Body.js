69c30e52ed0d14daec5ff36763b6092eb016def5
lingaraj2020 <lingarajhonasani2020@gmail.com>
2024-06-15 19:24:03 +0530

    added swiggy live api and updated top restaurants and search button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-const[ListofRestaurant,setListofRestaurant]=useState(resList);
-  return (
+  const [ListofRestaurant, setListofRestaurant] = useState([]);
+  const [filteredRestaurant, setFiltredRestaurant] = useState([]);
+
+  const [serachText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&collection=83649&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+    );
+    const json = await data.json();
+
+    //optional chaining
+    setListofRestaurant(json?.data?.cards?.slice(3));
+    setFiltredRestaurant(json?.data?.cards?.slice(3));
+  };
+
+
+  return  (ListofRestaurant.length===0?(<Shimmer/>):
     <div className="body">
       <div className="filter">
         <button
@@ -13,16 +33,35 @@ const[ListofRestaurant,setListofRestaurant]=useState(resList);
             const filteredList = ListofRestaurant.filter(
               (res) => res.card.card.info.avgRating > 4.2
             );
-            setListofRestaurant(filteredList);
+            setFiltredRestaurant(filteredList);
           }}
         >
           Top rated Restaurants
         </button>
-        <label>Search: </label>
-        <input type="text"></input>
+        <input
+          type="text"
+          value={serachText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={() => {
+            const filteredRestaurant = ListofRestaurant.filter((res) => {
+              return res.card.card.info.name
+                .toLowerCase()
+                .includes(serachText.toLowerCase());
+            });
+
+            setFiltredRestaurant(filteredRestaurant);
+          }}
+        >
+          {" "}
+          search
+        </button>
       </div>
       <div className="res-container">
-        {ListofRestaurant.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard
             key={restaurant.card.card.info.id}
             resData={restaurant}
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,7 +8,7 @@ const RestaurantCard = (props) => {
     avgRating,
     costForTwo,
     cloudinaryImageId,
-    sla: { deliveryTime },
+    sla,
   } = resData?.card?.card?.info;
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
@@ -21,7 +21,7 @@ const RestaurantCard = (props) => {
       <h4>{cuisines.join(",")}</h4>
       <h4>{avgRating} stars</h4>
       <h4>{costForTwo}</h4>
-      <h4>{deliveryTime} Minutes</h4>
+      <h4>{sla?.deliveryTime} Minutes</h4>
     </div>
   );
 };
